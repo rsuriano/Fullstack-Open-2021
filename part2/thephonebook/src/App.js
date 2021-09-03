120be6e46acdb198fabda51fbf9254f077bb8797
rsuriano <ramirosuriano@gmail.com>
2021-09-03 20:56:10 -0300

    2.8: The Phonebook Step3

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -3,27 +3,33 @@ import React, { useState } from 'react'
 const App = () => {
   const [ persons, setpersons ] = useState([
     {
-      name: 'Arto Hellas'
+      name: 'Arto Hellas',
+      phone: '555-12345'
     }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone ] = useState('')
   
   // Reads input value and updates its corresponding state
-  const handleInput = (event) => {
+  const handleName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumber = (event) => {
+    setNewPhone(event.target.value)
   }
 
   // Form Submit handler, adds the current newName to the list of persons
   const addPerson = (event) => {
     event.preventDefault()
 
-    console.log( persons.filter((p) => p.name===newName) )
     //Detect duplicate name and alerts the user
     if ( persons.filter((p) => p.name===newName).length > 0 ){
       window.alert(`${newName} is already on the phonebook.`)
     } else {
       const person = {
-        name: newName
+        name: newName,
+        phone: newPhone
       }  
       setpersons(persons.concat(person))
     }
@@ -33,16 +39,14 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input onChange={handleInput}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div> name: <input onChange={handleName}/> </div>
+        <div> number: <input onChange={handleNumber}/> </div>
+        <div> <button type="submit">add</button>  </div>
       </form>
 
       <h2>Numbers</h2>
-      {persons.map((person) => <ul key={person.name}>{person.name}</ul>)}
+      {persons.map((person) => <ul key={person.name}> {person.name} | {person.phone} </ul>)}
+      
     </div>
   )
 }

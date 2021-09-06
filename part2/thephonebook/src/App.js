dc027da2938f7e3188e352503c9fead620f7fec3
rsuriano <ramirosuriano@gmail.com>
2021-09-06 16:00:54 -0300

    2.9: The Phonebook Step4

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,29 +1,36 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setpersons ] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: '555-12345'
-    }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
+  const [ filterValue, setFilterValue] = useState('')
   
-  // Reads input value and updates its corresponding state
+  // Reads input name and updates its corresponding state
   const handleName = (event) => {
     setNewName(event.target.value)
   }
 
+  // Reads input phone number and updates its corresponding state
   const handleNumber = (event) => {
     setNewPhone(event.target.value)
   }
 
-  // Form Submit handler, adds the current newName to the list of persons
+  // Reads filter input and updates its corresponding state
+  const handleFilter = (event) => {
+    setFilterValue(event.target.value.toLowerCase())
+  }
+
+  // Form Submit handler, adds the current newName and newPhone to the list of persons
   const addPerson = (event) => {
     event.preventDefault()
 
-    //Detect duplicate name and alerts the user
+    //Detects duplicate name and alerts the user
     if ( persons.filter((p) => p.name===newName).length > 0 ){
       window.alert(`${newName} is already on the phonebook.`)
     } else {
@@ -31,13 +38,16 @@ const App = () => {
         name: newName,
         phone: newPhone
       }  
-      setpersons(persons.concat(person))
+      setPersons(persons.concat(person))
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>Filter by name: <input onChange={handleFilter}/></div>
+
       <form onSubmit={addPerson}>
         <div> name: <input onChange={handleName}/> </div>
         <div> number: <input onChange={handleNumber}/> </div>
@@ -45,7 +55,10 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map((person) => <ul key={person.name}> {person.name} | {person.phone} </ul>)}
+      {persons
+        .filter( ({name}) =>  name.toLowerCase().includes(filterValue) )
+        .map( (person) => <ul key={person.name}> {person.name} | {person.phone} </ul> )
+      }
       
     </div>
   )
